f43ba1759e28294191efb4bb759950c779b5ae7c
Rion66-byte <58502503+Rion66-byte@users.noreply.github.com>
2021-07-22 19:11:24 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const gettingElement1 = document.getElementById('yes1');
+gettingElement1.addEventListener('click', () => {
+    const love = document.getElementById('love');
+    love.style.display = 'none';
+    const loveYou = document.getElementById('loveYou');
+    loveYou.style.display = 'block';
+});
+
+const gettingElement2 = document.getElementById('yes2');
+gettingElement2.addEventListener('click', () => {
+    const love = document.getElementById('love');
+    love.style.display = 'none';
+    const loveYou = document.getElementById('loveYou');
+    loveYou.style.display = 'block';
+});
